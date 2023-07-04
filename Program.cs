a5b4c6415f64e1afaa06ff4d4d703677b6dbff40
kganitis <ganitiskostas@gmail.com>
2023-07-04 13:32:02 +0300

    Renamed form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp2023_Final
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WelcomeForm());
         }
     }
 }
