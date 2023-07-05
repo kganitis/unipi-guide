90b0e1fd3ad700cda3db052aa450639d4acd2ad1
kganitis <ganitiskostas@gmail.com>
2023-07-05 18:44:23 +0300

    Added base buttons

--- BaseForm.Designer.cs
+++ BaseForm.Designer.cs
@@ -28,38 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ButtonsTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.NavigationTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.BaseTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ListenButton = new System.Windows.Forms.Button();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.ButtonsTableLayoutPanel.SuspendLayout();
+            this.BaseTableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // ButtonsTableLayoutPanel
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 501);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.ButtonsTableLayoutPanel.ColumnCount = 4;
+            this.ButtonsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsTableLayoutPanel.Controls.Add(this.ExitButton, 3, 0);
+            this.ButtonsTableLayoutPanel.Controls.Add(this.ListenButton, 2, 0);
+            this.ButtonsTableLayoutPanel.Controls.Add(this.ExportButton, 1, 0);
+            this.ButtonsTableLayoutPanel.Controls.Add(this.AboutButton, 0, 0);
+            this.ButtonsTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ButtonsTableLayoutPanel.Location = new System.Drawing.Point(179, 428);
+            this.ButtonsTableLayoutPanel.Name = "ButtonsTableLayoutPanel";
+            this.ButtonsTableLayoutPanel.RowCount = 1;
+            this.ButtonsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ButtonsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.ButtonsTableLayoutPanel.Size = new System.Drawing.Size(702, 70);
+            this.ButtonsTableLayoutPanel.TabIndex = 1;
+            // 
+            // NavigationTableLayoutPanel
+            // 
+            this.NavigationTableLayoutPanel.ColumnCount = 1;
+            this.NavigationTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.NavigationTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NavigationTableLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            this.NavigationTableLayoutPanel.Name = "NavigationTableLayoutPanel";
+            this.NavigationTableLayoutPanel.RowCount = 5;
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.NavigationTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.NavigationTableLayoutPanel.Size = new System.Drawing.Size(170, 419);
+            this.NavigationTableLayoutPanel.TabIndex = 0;
+            // 
+            // BaseTableLayoutPanel
+            // 
+            this.BaseTableLayoutPanel.ColumnCount = 2;
+            this.BaseTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.BaseTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.BaseTableLayoutPanel.Controls.Add(this.NavigationTableLayoutPanel, 0, 0);
+            this.BaseTableLayoutPanel.Controls.Add(this.ButtonsTableLayoutPanel, 1, 1);
+            this.BaseTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BaseTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.BaseTableLayoutPanel.Name = "BaseTableLayoutPanel";
+            this.BaseTableLayoutPanel.RowCount = 2;
+            this.BaseTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.BaseTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.BaseTableLayoutPanel.Size = new System.Drawing.Size(884, 501);
+            this.BaseTableLayoutPanel.TabIndex = 0;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackgroundImage = global::WindowsFormsApp2023_Final.Properties.Resources.exit;
+            this.ExitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ExitButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ExitButton.Location = new System.Drawing.Point(545, 3);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(137, 64);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // ListenButton
+            // 
+            this.ListenButton.BackgroundImage = global::WindowsFormsApp2023_Final.Properties.Resources.speaker;
+            this.ListenButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ListenButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ListenButton.Location = new System.Drawing.Point(370, 3);
+            this.ListenButton.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.ListenButton.Name = "ListenButton";
+            this.ListenButton.Size = new System.Drawing.Size(135, 64);
+            this.ListenButton.TabIndex = 2;
+            this.ListenButton.UseVisualStyleBackColor = true;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.BackgroundImage = global::WindowsFormsApp2023_Final.Properties.Resources.save;
+            this.ExportButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ExportButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ExportButton.Location = new System.Drawing.Point(195, 3);
+            this.ExportButton.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(135, 64);
+            this.ExportButton.TabIndex = 1;
+            this.ExportButton.UseVisualStyleBackColor = true;
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.BackgroundImage = global::WindowsFormsApp2023_Final.Properties.Resources.users;
+            this.AboutButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AboutButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AboutButton.Location = new System.Drawing.Point(20, 3);
+            this.AboutButton.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(135, 64);
+            this.AboutButton.TabIndex = 0;
+            this.AboutButton.UseVisualStyleBackColor = true;
             // 
             // BaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 501);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.BaseTableLayoutPanel);
             this.Name = "BaseForm";
             this.Text = "Πανεπιστήμιο Πειραιώς";
+            this.ButtonsTableLayoutPanel.ResumeLayout(false);
+            this.BaseTableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel ButtonsTableLayoutPanel;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button ListenButton;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.TableLayoutPanel NavigationTableLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel BaseTableLayoutPanel;
     }
 }
 
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsApp2023_Final.Properties
-{
-
-
+namespace WindowsFormsApp2023_Final.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,154 @@ namespace WindowsFormsApp2023_Final.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsApp2023_Final.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap about {
+            get {
+                object obj = ResourceManager.GetObject("about", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap error {
+            get {
+                object obj = ResourceManager.GetObject("error", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap exit {
+            get {
+                object obj = ResourceManager.GetObject("exit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap file {
+            get {
+                object obj = ResourceManager.GetObject("file", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap home {
+            get {
+                object obj = ResourceManager.GetObject("home", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap mute {
+            get {
+                object obj = ResourceManager.GetObject("mute", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap save {
+            get {
+                object obj = ResourceManager.GetObject("save", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap speaker {
+            get {
+                object obj = ResourceManager.GetObject("speaker", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap speaker_2 {
+            get {
+                object obj = ResourceManager.GetObject("speaker_2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap stop {
+            get {
+                object obj = ResourceManager.GetObject("stop", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap users {
+            get {
+                object obj = ResourceManager.GetObject("users", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
