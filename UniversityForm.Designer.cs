ff957d0f9d480dade261d3861ccb20370ffaa69d
kganitis <ganitiskostas@gmail.com>
2023-07-06 17:42:40 +0300

    Renamed nav buttons

---
+++ UniversityForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp2023_Final
+{
+    partial class UniversityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // NavButton1
+            // 
+            this.NavButton1.BackColor = System.Drawing.Color.BurlyWood;
+            // 
+            // NavButton5
+            // 
+            this.NavButton5.Text = "";
+            // 
+            // NavButton4
+            // 
+            this.NavButton4.Text = "";
+            // 
+            // NavButton3
+            // 
+            this.NavButton3.Text = "";
+            // 
+            // NavButton2
+            // 
+            this.NavButton2.Text = "";
+            // 
+            // UniversityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Name = "UniversityForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
