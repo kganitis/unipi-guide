7efc280e8866a80780f749f5667ffb464999ab86
kganitis <ganitiskostas@gmail.com>
2023-07-06 15:02:34 +0300

    Hotfix: corrected login form size

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -69,7 +69,6 @@
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.ClientSize = new System.Drawing.Size(944, 537);
             this.Name = "LoginForm";
             this.ResumeLayout(false);
 
