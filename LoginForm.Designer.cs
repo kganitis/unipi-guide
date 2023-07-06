a06b16b398b75ad56ac428551b4189a006eeb203
Stergios9 <tselios1990@gmail.com>
2023-07-06 20:24:56 +0300

    Designed all the guide forms

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -28,16 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ContentPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Controls.Add(this.textBox1);
+            this.ContentPanel.Controls.Add(this.button1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(422, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 26);
+            this.textBox1.TabIndex = 1;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.ClientSize = new System.Drawing.Size(1476, 863);
+            this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "LoginForm";
+            this.ContentPanel.ResumeLayout(false);
+            this.ContentPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
