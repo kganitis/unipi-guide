e2deafbf8834df032a0310c387c3edd09cd427f7
kganitis <ganitiskostas@gmail.com>
2023-07-07 17:15:49 +0300

    Added basic functionality to back button in GuideSectionForm

---
+++ SchoolsSectionForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2023_Final
+{
+    public partial class SchoolsSectionForm : WindowsFormsApp2023_Final.GuideSectionForm
+    {
+        public SchoolsSectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NavButtonBack_Click(object sender, EventArgs e)
+        {
+            Close();
+            new GuideIntroForm().Show();
+        }
+    }
+}
---
+++ ServicesSectionForm.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace WindowsFormsApp2023_Final
+{
+    partial class ServicesSectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // NavButton1
+            // 
+            this.NavButton1.Text = "Υπηρεσίες";
+            // 
+            // NavButtonBack
+            // 
+            this.NavButtonBack.Click += new System.EventHandler(this.NavButtonBack_Click);
+            // 
+            // ServicesSectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Name = "ServicesSectionForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
