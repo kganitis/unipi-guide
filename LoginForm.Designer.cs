75f07801b2cb3d8b4e50bf8254d53a7462e48c95
Stergios9 <tselios1990@gmail.com>
2023-07-07 11:58:19 +0300

    Επεξεργασία φορμας login

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -28,40 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.ContentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // ContentPanel
             // 
-            this.ContentPanel.Controls.Add(this.textBox1);
+            this.ContentPanel.Controls.Add(this.button2);
             this.ContentPanel.Controls.Add(this.button1);
+            this.ContentPanel.Controls.Add(this.textBox2);
+            this.ContentPanel.Controls.Add(this.textBox1);
+            this.ContentPanel.Controls.Add(this.label2);
+            this.ContentPanel.Controls.Add(this.label1);
+            this.ContentPanel.Controls.Add(this.pictureBox6);
+            this.ContentPanel.Controls.Add(this.pictureBox5);
+            this.ContentPanel.Dock = System.Windows.Forms.DockStyle.None;
+            this.ContentPanel.Location = new System.Drawing.Point(300, 37);
+            this.ContentPanel.Size = new System.Drawing.Size(996, 530);
+            this.ContentPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.ContentPanel_Paint);
             // 
-            // button1
+            // ExportButton
             // 
-            this.button1.Location = new System.Drawing.Point(109, 140);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(206, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.ExportButton.Size = new System.Drawing.Size(100, 123);
+            // 
+            // ListenButton
+            // 
+            this.ListenButton.Location = new System.Drawing.Point(508, 15);
+            this.ListenButton.Size = new System.Drawing.Size(124, 123);
+            // 
+            // NavButton1
+            // 
+            this.NavButton1.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.NavButton1.Size = new System.Drawing.Size(219, 92);
+            // 
+            // NavButton5
+            // 
+            this.NavButton5.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.NavButton5.Size = new System.Drawing.Size(219, 92);
+            // 
+            // NavButton4
+            // 
+            this.NavButton4.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.NavButton4.Size = new System.Drawing.Size(219, 92);
+            // 
+            // NavButton3
+            // 
+            this.NavButton3.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.NavButton3.Size = new System.Drawing.Size(219, 92);
+            // 
+            // NavButton2
+            // 
+            this.NavButton2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.NavButton2.Size = new System.Drawing.Size(219, 92);
+            // 
+            // NavButtonHome
+            // 
+            this.NavButtonHome.Location = new System.Drawing.Point(18, 13);
+            this.NavButtonHome.Size = new System.Drawing.Size(219, 123);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::WindowsFormsApp2023_Final.Properties.Resources.pic12;
+            this.pictureBox5.Location = new System.Drawing.Point(0, -13);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(779, 228);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::WindowsFormsApp2023_Final.Properties.Resources.pic41;
+            this.pictureBox6.Location = new System.Drawing.Point(776, 0);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(408, 215);
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(89, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Όνομα Χρήστη";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(89, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Κωδικός";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(422, 176);
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox1.Location = new System.Drawing.Point(321, 253);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 26);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(237, 35);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox2.Location = new System.Drawing.Point(321, 306);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 35);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.Location = new System.Drawing.Point(94, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 70);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Είσοδος Χρήστη";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button2.Location = new System.Drawing.Point(756, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 70);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Είσοδος Επισκέπτη";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.ClientSize = new System.Drawing.Size(1476, 863);
+            this.ClientSize = new System.Drawing.Size(1339, 727);
             this.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ContentPanel.ResumeLayout(false);
             this.ContentPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,7 +190,17 @@
 
         #endregion
 
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
