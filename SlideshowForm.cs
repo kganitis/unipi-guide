89571ebf76d0ee481d4e6159a0fece86f3f5c4b5
kganitis <ganitiskostas@gmail.com>
2023-07-08 18:01:11 +0300

    Designed SlideshowForm
    - Created the SlideshowForm form, which inherits the GuideForm.
    - Highlighted the corresponding button in the navigation menu.

---
+++ SlideshowForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2023_Final
+{
+    public partial class SlideshowForm : WindowsFormsApp2023_Final.GuideForm
+    {
+        public SlideshowForm()
+        {
+            InitializeComponent();
+            NavButton5.BackColor = highlightedButtonColor;
+        }
+    }
+}
