1f4a78da12c67029230de6ecd93705521b28eee7
kganitis <ganitiskostas@gmail.com>
2023-07-08 17:59:09 +0300

    Designed ReviewsForm
    - Created the ReviewsForm form, which inherits the GuideForm.
    - Highlighted the corresponding button in the navigation menu.

---
+++ ReviewsForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WindowsFormsApp2023_Final
+{
+    partial class ReviewsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // NavButton4
+            // 
+            this.NavButton4.BackColor = System.Drawing.Color.BurlyWood;
+            // 
+            // ReviewsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Name = "ReviewsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ ReviewsForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2023_Final
+{
+    public partial class ReviewsForm : WindowsFormsApp2023_Final.GuideForm
+    {
+        public ReviewsForm()
+        {
+            InitializeComponent();
+            NavButton4.BackColor = highlightedButtonColor;
+        }
+    }
+}
