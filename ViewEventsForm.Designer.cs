3e44f7e8174d2dcc2fda0d689d8f5ce294e50249
GiorgosSimos <giorgos_Simos@windowslive.com>
2023-08-06 22:43:15 +0300

    Commit Changes

--- ViewEventsForm.Designer.cs
+++ ViewEventsForm.Designer.cs
@@ -29,12 +29,16 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label_desc = new System.Windows.Forms.Label();
+            this.label_date = new System.Windows.Forms.Label();
             this.ContentPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // ContentPanel
             // 
             this.ContentPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ContentPanel.Controls.Add(this.label_date);
+            this.ContentPanel.Controls.Add(this.label_desc);
             this.ContentPanel.Controls.Add(this.label1);
             this.ContentPanel.Size = new System.Drawing.Size(995, 539);
             // 
@@ -46,22 +50,34 @@
             // NavButton5
             // 
             this.NavButton5.Enabled = false;
-            this.NavButton5.Text = "Καταχώρηση";
+            this.NavButton5.Location = new System.Drawing.Point(13, 493);
+            this.NavButton5.Size = new System.Drawing.Size(240, 45);
+            this.NavButton5.Text = "Άλλο";
+            this.NavButton5.Visible = false;
             this.NavButton5.Click += new System.EventHandler(this.NavButton5_Click);
             // 
             // NavButton4
             // 
-            this.NavButton4.Text = "Ημερίδες";
+            this.NavButton4.Enabled = false;
+            this.NavButton4.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.NavButton4.Location = new System.Drawing.Point(14, 385);
+            this.NavButton4.Size = new System.Drawing.Size(240, 100);
+            this.NavButton4.Text = "Νέα Δραστηριότητα";
             this.NavButton4.Click += new System.EventHandler(this.NavButton4_Click);
             // 
             // NavButton3
             // 
-            this.NavButton3.Text = "Δράσεις";
+            this.NavButton3.Font = new System.Drawing.Font("Arial Narrow", 18F);
+            this.NavButton3.Location = new System.Drawing.Point(13, 330);
+            this.NavButton3.Size = new System.Drawing.Size(240, 47);
+            this.NavButton3.Text = "Ημερολόγιο";
             this.NavButton3.Click += new System.EventHandler(this.NavButton3_Click);
             // 
             // NavButton2
             // 
-            this.NavButton2.Text = "Συνέδρια";
+            this.NavButton2.Font = new System.Drawing.Font("Arial Narrow", 18F);
+            this.NavButton2.Size = new System.Drawing.Size(240, 102);
+            this.NavButton2.Text = "Προβολή Δραστηριοτήτων";
             this.NavButton2.Click += new System.EventHandler(this.NavButton2_Click);
             // 
             // label1
@@ -74,6 +90,24 @@
             this.label1.Text = "Δραστηριότητες";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label_desc
+            // 
+            this.label_desc.AutoSize = true;
+            this.label_desc.Location = new System.Drawing.Point(132, 139);
+            this.label_desc.Name = "label_desc";
+            this.label_desc.Size = new System.Drawing.Size(44, 16);
+            this.label_desc.TabIndex = 1;
+            this.label_desc.Text = "label2";
+            // 
+            // label_date
+            // 
+            this.label_date.AutoSize = true;
+            this.label_date.Location = new System.Drawing.Point(475, 261);
+            this.label_date.Name = "label_date";
+            this.label_date.Size = new System.Drawing.Size(44, 16);
+            this.label_date.TabIndex = 2;
+            this.label_date.Text = "label2";
+            // 
             // ViewEventsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -82,6 +116,7 @@
             this.Name = "ViewEventsForm";
             this.Text = "ViewEventsForm";
             this.ContentPanel.ResumeLayout(false);
+            this.ContentPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,5 +125,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_desc;
+        private System.Windows.Forms.Label label_date;
     }
 }
--- ViewEventsForm.cs
+++ ViewEventsForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,27 +13,82 @@ namespace WindowsFormsApp2023_Final
 {
     public partial class ViewEventsForm : WindowsFormsApp2023_Final.GuideSectionForm
     {
+        UserSession session = UserSession.Instance;
+
+        String connectionString = "Data source=rad19900.db;Version=3;";
+        SQLiteConnection connection;
+
+        string description, date;
+
+
         public ViewEventsForm()
         {
             InitializeComponent();
         }
 
+        private void myClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi!");
+        }
+
         protected override void NavButton2_Click(object sender, EventArgs e)
         {
-            label1.Text = "Συνέδρια";
+            label1.Text = "Όλες οι Δραστηριότητες";
+           
+            connection = new SQLiteConnection(connectionString);
+            connection.Open();
+            String selectSQL = "select description, date from event";
+            SQLiteCommand command = new SQLiteCommand(selectSQL, connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            int i = 0;
+            while (reader.Read())
+            {
+                description = reader.GetString(0);
+                date = reader.GetString(1);
+
+                Label label_desc = new Label();
+                label_desc.AutoSize = true;
+                label_desc.Font = new Font("Modern No. 20", 13.8F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+                label_desc.BackColor = Color.Maroon;
+                label_desc.ForeColor = Color.Transparent;
+                label_desc.Location = new Point(100, 45 + i * label_desc.Height);
+                label_desc.Name = "descreption";
+                label_desc.Size = new Size(192, 132);
+                label_desc.Text = description;
+                label_desc.Click += new EventHandler(myClick);
+                Controls.Add(label_desc);
+
+                Label label_date = new Label();
+                label_date.AutoSize = true;
+                label_date.Font = new Font("Modern No. 20", 13.8F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+                label_date.ForeColor = Color.Maroon;
+                label_date.Location = new Point(100, 45 + i * label_date.Height);
+                label_date.Name = "date";
+                label_date.Size = new Size(32, 32);
+                label_date.Text = date;
+                label_date.Click += new EventHandler(myClick);
+                Controls.Add(label_date);
+
+                i++;
+                //label2.Text += date + "\n";
+                //label3.Text += description + "\n";
+            }
+            reader.Close();
+            command.Dispose();
+            connection.Close();
         }
 
         protected override void NavButton3_Click(object sender, EventArgs e)
         {
-            label1.Text = "Δράσεις";
+            label1.Text = "Ημερολόγιο";
         }
 
         protected override void NavButton4_Click(object sender, EventArgs e)
         {
-            label1.Text = "Ημερίδες";
+            label1.Text = "Καταχώρηση";
         }
 
-        protected override void NavButton5_Click(object sender, EventArgs e)
+        protected override void NavButton5_Click(object sender, EventArgs e)// μόνο για εγγεγραμένους χρήστες
         {
             label1.Text = "Καταχώρηση νέου Event";
         }
